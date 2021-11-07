e28253833045f1ce3f40dd70cdb35fa4d38c5596
Ameer <Amir.abh1996@gmail.com>
2021-11-07 12:25:11 -0500

    commit tree 4887b6c40506b5fb3d05ebdda8bf7547f7a89659

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(driversArray) {
+    return driversArray.slice(0,2)
+}
+function returnLastTwoDrivers(driversArray) {
+    return driversArray.slice(2, 4)
+}
